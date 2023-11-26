9ad4a8570753dab26cc1d4b5223cfbafa01d3803
Jonatas Rodriguez <jonatasmviscaino@gmail.com>
2023-11-26 13:09:54 +0100

    Updates Day 27

--- Day 27 - Mile to Kilometers Converter/main.py
+++ Day 27 - Mile to Kilometers Converter/main.py
@@ -3,27 +3,32 @@ from tkinter import *
 # Creating window object
 window = Tk()
 window.title("Mile to Kilometer Converter")
-window.minsize(width=500, height=300)
+window.minsize(width=250, height=100)
+window.config(padx=20, pady=20)
 
-# Label
-my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label.pack()
+# Labels
+FONT = ("Arial", 12)
+miles_label = Label(text="Miles", font=FONT)
+miles_label.grid(row=0, column=2)
+Km_label = Label(text="Km", font=FONT)
+Km_label.grid(row=1, column=2)
+equal_label = Label(text="is equal to", font=FONT)
+equal_label.grid(row=1, column=0)
+result_label = Label(text="", font=FONT)
+result_label.grid(row=1, column=1)
 
 # Button
+def calculate():
+    km_text = float(input_value.get())*1.60934
+    result_label.config(text=f"{km_text}")
 
-def click():
-    new_text = input.get()
-    my_label.config(text=f"{new_text}")
 
-button = Button(text="Click Me", command=click)
-button.pack()
+calculate_button = Button(text="Calculate", command=calculate)
+calculate_button.grid(row=2, column=1)
 
 # Entry
-input = Entry()
-input.pack()
-
-
-
+input_value = Entry(width=8)
+input_value.focus()
+input_value.grid(row=0, column=1)
 
 window.mainloop()
-
