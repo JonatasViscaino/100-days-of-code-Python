bb602308d89fa7ca31d641bb5fc0863c65871a63
Jonatas Rodriguez <jonatasmviscaino@gmail.com>
2023-11-29 12:26:06 +0100

    Improvement Day 26

--- Day 26 - The NATO Alphabet/main.py
+++ Day 26 - The NATO Alphabet/main.py
@@ -4,7 +4,17 @@ import pandas as pd
 df = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 
+
 # Generating final result to return NATO Alphabet
-input_word = input("Please enter a word: ")
-result_list = [nato_dict[letter.upper()] for letter in input_word]
-print(result_list)
+def generating_nato():
+    input_word = input("Please enter a word: ")
+    try:
+        result_list = [nato_dict[letter.upper()] for letter in input_word]
+    except KeyError:
+        print("Sorry, please enter only letters in the alphabet please.")
+        generating_nato()
+    else:
+        print(result_list)
+
+
+generating_nato()
