efb5291c13ee649d3a6779e3f92bb599386f4cec
Jonatas Rodriguez <jonatasmviscaino@gmail.com>
2023-12-10 19:15:38 +0100

    Day 36 - Final Version

--- Day 36 - Stock News Alert/README.md
+++ Day 36 - Stock News Alert/README.md
@@ -1,1 +1,19 @@
-# Day 36 - Stock News Alert
+# Day 36 - Stock News Alert Project
+
+## Overview
+This project is a simple yet effective Stock News Alert system that allows users to track and receive timely updates on the stock of their choice. The user can specify a stock symbol, and the system will send an email alert when the stock's variance exceeds 2% based on the latest available data. In addition to stock information, the email includes relevant news articles about the associated company.
+
+## How It Works
+The system utilizes two external APIs - Alpha Vantage for retrieving daily stock data and News API for fetching recent news related to the specified company. The user-configurable parameters such as stock symbol, company name, email credentials, and API keys are set as constants.
+
+To determine the stock variance, the system compares the closing prices of the latest two available days, calculating the percentage change. If the variance is greater than or equal to 2%, an email alert is triggered.
+
+The email notification includes an emoji indicating whether the stock is trending upward or downward, the percentage change, and the top three recent news articles related to the company.
+
+## Configuration
+Make sure to fill in the constants at the beginning of the script with your own values, including your stock API key, news API key, email credentials, and the stock symbol you want to track.
+
+## Dependencies
+This project uses the requests library for making API calls and the smtplib library for sending emails. Ensure these libraries are installed before running the script.
+
+Feel free to customize and enhance the script to suit your preferences or integrate additional features. Happy tracking!
--- Day 36 - Stock News Alert/main.py
+++ Day 36 - Stock News Alert/main.py
@@ -7,38 +7,50 @@ from email.message import EmailMessage
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
-STOCK_API = "8PQY1VQG0RMTSYM0"
+STOCK_API = "XXXXX"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
-NEWS_API = "42c6b37f816d4cfd8d82dea21c88b8f5"
-MY_EMAIL = "jonatasmviscaino@gmail.com"
-GMAIL_PASS = "XXXX"
+NEWS_API = "XXXXX"
+MY_EMAIL = "XXXXX@gmail.com"
+GMAIL_PASS = "XXXXXXXXXXXXXXXXXX"
 
 # Date Values
-yesterday_date = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
-bef_yesterday_date = datetime.strftime(datetime.now() - timedelta(2), '%Y-%m-%d')
+# For Sunday, it will select the latest stocks of Thursday and Friday
+if datetime.now().weekday() == 6:
+    yesterday_date = datetime.strftime(datetime.now() - timedelta(2), '%Y-%m-%d')
+    bef_yesterday_date = datetime.strftime(datetime.now() - timedelta(3), '%Y-%m-%d')
+# For Monday, it will select the latest stock of Thursday and Friday
+elif datetime.now().weekday() == 0:
+    yesterday_date = datetime.strftime(datetime.now() - timedelta(3), '%Y-%m-%d')
+    bef_yesterday_date = datetime.strftime(datetime.now() - timedelta(4), '%Y-%m-%d')
+# For Tuesday, it will select the latest stock of Thursday and Friday
+elif datetime.now().weekday() == 1:
+    yesterday_date = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
+    bef_yesterday_date = datetime.strftime(datetime.now() - timedelta(4), '%Y-%m-%d')
+else:
+    yesterday_date = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
+    bef_yesterday_date = datetime.strftime(datetime.now() - timedelta(2), '%Y-%m-%d')
 
 # Getting data from STOCK API:
-# stock_parameters = {
-#     "function": "TIME_SERIES_DAILY",
-#     "symbol": STOCK,
-#     "apikey": STOCK_API,
-# }
-#
-# response = requests.get(url=STOCK_ENDPOINT, params=stock_parameters)
-# response.raise_for_status()
-# data = response.json()["Time Series (Daily)"]
-#
-# yest_close_price = float(data[yesterday_date]["4. close"])
-# bef_yest_close_price = float(data[bef_yesterday_date]["4. close"])
-# diff = yest_close_price - bef_yest_close_price
-# diff_percent = round((diff/yest_close_price)*100)
+stock_parameters = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK,
+    "apikey": STOCK_API,
+}
+
+response = requests.get(url=STOCK_ENDPOINT, params=stock_parameters)
+response.raise_for_status()
+data = response.json()["Time Series (Daily)"]
+
+yest_close_price = float(data[yesterday_date]["4. close"])
+bef_yest_close_price = float(data[bef_yesterday_date]["4. close"])
+diff = yest_close_price - bef_yest_close_price
+diff_percent = round((diff/yest_close_price)*100, 2)
 
 # Creating Emoji for E-mail
-up_down = "TEST"
-# if diff > 0:
-#     up_down = "UP"
-# else:
-#     up_down = "DOWN"
+if diff > 0:
+    up_down = "📈"
+else:
+    up_down = "📉"
 
 # Getting data from NEWS API:
 news_parameters = {
@@ -51,18 +63,25 @@ response = requests.get(url=NEWS_ENDPOINT, params=news_parameters)
 response.raise_for_status()
 articles = response.json()['articles']
 
-email_body_list = [(f"{STOCK}\nHeadline: {article['title']}\n\n"
-                    f"Description: {article['description']}\n\nUrl: {article['url']}") for article in articles[:3]]
+email_body_list = []
+for article in articles[:3]:
+    headline = article["title"]
+    description = article["description"]
+    url = article["url"]
+    date = datetime.fromisoformat(article["publishedAt"]).strftime('%d-%m-%Y')
+    body_string = (f"\nHEADLINE: {headline}\nPUBLISHED DATE: {date}\n"
+                   f"DESCRIPTION: {article['description']}\nURL: {article['url']}\n")
+    email_body_list.append(body_string)
 
-#if abs(diff_percent) >= 0:
-subject = f"{COMPANY_NAME}:{up_down} %"
-body = '\n'.join(email_body_list)
-message = EmailMessage()
-message.add_header("From", MY_EMAIL)
-message.add_header("To", MY_EMAIL)
-message.add_header("Subject", subject)
-message.set_payload(body, "utf-8")
-with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-    connection.starttls()
-    connection.login(user=MY_EMAIL, password=GMAIL_PASS)
-    connection.sendmail(msg=message, to_addrs=MY_EMAIL, from_addr=MY_EMAIL)
+if abs(diff_percent) >= 2:
+    subject = f"{COMPANY_NAME}: {STOCK} {up_down} {diff_percent}%"
+    body = '\n'.join(email_body_list)
+    message = EmailMessage()
+    message.add_header("From", MY_EMAIL)
+    message.add_header("To", MY_EMAIL)
+    message.add_header("Subject", subject)
+    message.set_payload(body, "utf-8")
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=GMAIL_PASS)
+        connection.send_message(message, to_addrs=MY_EMAIL, from_addr=MY_EMAIL)
