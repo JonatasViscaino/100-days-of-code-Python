17d90d8d131c7c0cfbaebb992f925cc392d91ca6
Jonatas Rodriguez <jonatasmviscaino@gmail.com>
2023-12-12 12:31:48 +0100

    Day 37 - Final Update

---
+++ Day 37 - Habit tracker/README.md
@@ -0,0 +1,15 @@
+# Day 37 - Pixela habit tracker
+
+This Python script leverages the Pixela API (https://pixe.la/) to create a habit tracker, allowing users to visualize and track their daily habits. Key features include:
+
+**User Creation**: Easily create a user account using the Pixela API by providing a username, token, and agreeing to the terms of service.
+
+**Graph Creation**: Set up personalized graphs to track habits over time. Define graph properties such as name, unit, type, and color.
+
+**Pixel Creation and Updates**: Log daily habits as pixels on the graph with specified quantities. Update existing pixels to reflect changes in habit progress.
+
+**Pixel Deletion**: Remove unwanted or incorrect habit data by deleting pixels for specific dates.
+
+To use, replace "YOUR_USERNAME", "YOUR_CREATED_TOKEN" and "YOUR_GRAPH_NAME" with your Pixela account details. Uncomment the relevant sections to execute user, graph, and pixel-related actions.
+
+Note: Ensure proper handling of sensitive information like your Pixela token.
--- Day 37 - Habit tracker/main.py
+++ Day 37 - Habit tracker/main.py
@@ -1,17 +1,69 @@
 import requests
+from datetime import datetime
 
-pixela_user_endpoint = "https://pixe.la/v1/users"
+USERNAME = "YOUR_USERNAME"
+TOKEN = "YOUR_CREATED_TOKEN"
+GRAPH_ID = "YOUR_GRAPH_NAME"
+
+pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "YOUR_TOKEN",
-    "username": "YOUR_USERNAME",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes/no",
     "notMinor": "yes/no",
 }
 
 # Creating a user
-#response = requests.post(url=pixela_endpoint, json=user_params)
-#print(response.text)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
 
-graph_endpoint = "https://pixe.la/v1/users/tinhas99/graphs"
+# Creating a graph
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
+graph_params = {
+    "id": GRAPH_ID,
+    "name": "Program Graph",
+    "unit": "Hours",
+    "type": "float",
+    "color": "sora",
+}
+
+graph_headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_params, headers=graph_headers)
+# print(response.text)
+
+# Creating a Habit Pixel in graph
+pixelcreation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today_date = datetime.now().strftime("%Y%m%d")
+
+pixel_data = {
+    "date": today_date,
+    "quantity": "2",
+}
+
+response = requests.post(url=pixelcreation_endpoint, headers=graph_headers, json=pixel_data)
+print(response.text)
+
+# Update a Habit pixel in graph
+day_update = "20231212"
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{day_update}"
+
+pixel_update = {
+    "quantity": "3"
+}
+
+# response = requests.put(url=update_endpoint, headers=graph_headers, json=pixel_update)
+# print(response.text)
+
+# Delete a Habit pixel data in graph
+day_delete = "20231212"
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{day_delete}"
+
+# response = requests.delete(url=delete_endpoint, headers=graph_headers)
+# print(response.text)
